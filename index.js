81907a3bebe1e399e4cc8b2d1f99508aa704894f
Kushagra Tiwari <kushagratiwariii03@gmail.com>
2026-03-26 14:17:57 +0530

    feat: build main application logic with Express, including routing, middleware, and CRUD operations for chat system

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ main()
     .then(() => {
         console.log("Connection successful!");
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err)); 
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
