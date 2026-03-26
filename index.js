b80d351f4da8db016144a77618949858bf52bc32
Kushagra Tiwari <kushagratiwariii03@gmail.com>
2026-03-26 14:14:42 +0530

    feat: finalize mini chat application with full CRUD operations, EJS views, and MongoDB integration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); 
 app.use(express.urlencoded ({ extended : true }));
 app.use(methodOverride("_method"));
 
